dba0ab40fec0fe9df089693e731b5fc3104c8f3e
fibonacci998 <fibonacci998@gmail.com>
2025-02-19 10:56:00 +0700

    feat: add sample to view point of student

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,15 +1,18 @@
 package com.example.demo;
 
+import java.math.BigInteger;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.web3j.crypto.Credentials;
 import org.web3j.protocol.Web3j;
 import org.web3j.protocol.core.methods.response.EthBlockNumber;
 import org.web3j.protocol.core.methods.response.Web3ClientVersion;
 import org.web3j.protocol.http.HttpService;
+import org.web3j.tx.gas.ContractGasProvider;
+import org.web3j.tx.gas.DefaultGasProvider;
 @SpringBootApplication
 public class DemoApplication {
-
-	private Point pointContract;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
@@ -18,9 +21,20 @@ public class DemoApplication {
 
 		// String infuraUrl = "https://mainnet.infura.io/v3/YOUR_INFURA_PROJECT_ID";
 		// String infuraUrl = "https://eth-mainnet.public.blastapi.io";
-		String infuraUrl = "https://jsonrpc.euphoria.aura.network";
+		String rpc = "https://jsonrpc.euphoria.aura.network";
 
-        Web3j web3j = Web3j.build(new HttpService(infuraUrl));
+		String privateKey = "562a7583e538b39cc3b43b6e6f9b9a1230e12dee5b8b104d1ab8d4d84199ae57";
+		String contractAddress = "0xCfEc2a7147413985eD1DD8064c2763165FB93d04";
+		Credentials credentials = Credentials.create(privateKey);
+		ContractGasProvider contractGasProvider = new DefaultGasProvider();
+        Web3j web3j = Web3j.build(new HttpService(rpc));
+		Point contract = Point.load(
+			contractAddress, 
+			web3j, 
+			credentials, 
+			contractGasProvider
+		);
+
         try {
             Web3ClientVersion clientVersion = web3j.web3ClientVersion().send();
             System.out.println("Connected to Ethereum Node: " + clientVersion.getWeb3ClientVersion());
@@ -29,6 +43,10 @@ public class DemoApplication {
 			.sendAsync()
 			.get();
 			System.err.println(result.getBlockNumber());
+
+			BigInteger studentPoint = contract.getPoints("0x51994393C4D23e2c245E7896289abeF9590F71a9", "math").send();
+
+			System.out.println("Student point: " + studentPoint);
         } catch (Exception e) {
             e.printStackTrace();
         }
